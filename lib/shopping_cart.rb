b839a8d92cb4305a1fac13bddd1c0e7605f4a42d
Leah Riffell <leah.riffell@gmail.com>
2020-07-08 09:15:18 -0600

    Set up shopping cart class and initialize method

---
+++ lib/shopping_cart.rb
@@ -0,0 +1,9 @@
+class ShoppingCart
+  attr_reader :name, :capacity, :products
+
+  def initialize(name, capacity, products = [])
+    @name = name
+    @capacity = capacity
+    @products = products
+  end
+end
---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/product'
+require '../lib/shopping_cart'
+
+class ShoppingCartTest < MiniTest::Test
+  def test_it_exists
+    cart = ShoppingCart.new("King Soopers", "30items")
+
+    assert_instance_of ShoppingCart, cart
+  end
+end
