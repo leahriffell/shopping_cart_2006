e70d2dd246bdcce10c47b12cb92fb6e4d52555a3
Leah Riffell <leah.riffell@gmail.com>
2020-07-08 09:09:12 -0600

    Adda method to see whether or not a product is hoarded

--- lib/product.rb
+++ lib/product.rb
@@ -1,14 +1,23 @@
 class Product
   attr_reader :category, :name, :unit_price, :quantity
 
-  def initialize(category, name, unit_price, quantity)
+  def initialize(category, name, unit_price, quantity, is_hoarded = false)
     @category = category
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @is_hoarded = is_hoarded
   end
 
   def total_price
     @unit_price * @quantity
+  end
+
+  def is_hoarded?
+    @is_hoarded
+  end
+
+  def hoard
+
   end
 end
