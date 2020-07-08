183a40e96dcde5a32cbb16e11b67883130b3d960
Leah Riffell <leah.riffell@gmail.com>
2020-07-08 09:03:04 -0600

    Create initialize method for product class

--- test/product_test.rb
+++ test/product_test.rb
@@ -1,6 +1,6 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/product'
+require '../lib/product'
 
 class ProductTest < Minitest::Test
   #Iteration1
@@ -10,7 +10,7 @@ class ProductTest < Minitest::Test
   end
 
   def test_it_has_a_category
-    skip
+    # skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal :paper, product.category
   end
